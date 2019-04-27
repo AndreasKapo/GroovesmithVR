8c86b079ec2c3dd2ba919be026a42b95cb98c5b2
Dan <danielswieca@gmail.com>
2019-04-27 10:01:32 -0400

    Added audio on hits

---
+++ VRLWRP/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance = null;
+    AudioSource audioSource;
+
+    public AudioClip[] goodAnvilHits;
+    public AudioClip[] badAnvilHits;
+
+    AudioClip previousGoodAnvilHit;
+    AudioClip previousBadAnvilHit;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGoodAnvilHit()
+    {
+        AudioClip hitSound;
+        do
+        {
+            hitSound = goodAnvilHits[Random.Range(0, goodAnvilHits.Length - 1)];
+        } while (hitSound == previousGoodAnvilHit);
+
+        previousGoodAnvilHit = hitSound;
+        PlaySound(hitSound);
+    }
+
+    public void PlayBadAnvilHit()
+    {
+        AudioClip hitSound;
+        do
+        {
+            hitSound = badAnvilHits[Random.Range(0, badAnvilHits.Length - 1)];
+        } while (hitSound == previousBadAnvilHit);
+
+        previousBadAnvilHit = hitSound;
+        PlaySound(hitSound);
+    }
+
+    void PlaySound(AudioClip clip)
+    {
+        audioSource.Stop();
+        audioSource.PlayOneShot(clip);
+    }
+}
